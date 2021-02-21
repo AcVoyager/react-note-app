da0937a0e04c6c3da514fb6359bd5635a8bfdad6
acvoyager <xcvoyager01@gmail.com>
2021-02-21 00:01:20 -0800

    ready to deployment

---
+++ docs/precache-manifest.0b65c3a8b676f871678fff94f6520523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1cd75b16e4dc0ab92a4",
+    "url": "/cs7580sp21-seattle/chang_xu_assignment2/static/css/main.846ce828.chunk.css"
+  },
+  {
+    "revision": "c1cd75b16e4dc0ab92a4",
+    "url": "/cs7580sp21-seattle/chang_xu_assignment2/static/js/main.c1cd75b1.chunk.js"
+  },
+  {
+    "revision": "e68bf5d27c0757b1a3ee",
+    "url": "/cs7580sp21-seattle/chang_xu_assignment2/static/js/1.e68bf5d2.chunk.js"
+  },
+  {
+    "revision": "697f780a245eb9e8dc86",
+    "url": "/cs7580sp21-seattle/chang_xu_assignment2/static/css/2.48861c26.chunk.css"
+  },
+  {
+    "revision": "697f780a245eb9e8dc86",
+    "url": "/cs7580sp21-seattle/chang_xu_assignment2/static/js/2.697f780a.chunk.js"
+  },
+  {
+    "revision": "9b5bf1e73a4a4a31b04a",
+    "url": "/cs7580sp21-seattle/chang_xu_assignment2/static/js/runtime~main.9b5bf1e7.js"
+  },
+  {
+    "revision": "f4f380aea7a8d710af65b2b647122ebc",
+    "url": "/cs7580sp21-seattle/chang_xu_assignment2/index.html"
+  }
+];
